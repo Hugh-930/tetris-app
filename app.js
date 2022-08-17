7fcf70485bac5f30d79fa0694d32fbe54375dd25
Hugh <hughhanderson@icloud.com>
2022-08-18 01:41:20 +0900

    added Block Class and Mino Class

---
+++ app.js
@@ -0,0 +1,15 @@
+class Block {
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class Mino {
+    constructor(x, y, rot, shape){
+        this.x = x;
+        this.y = y;
+        this.rot = rot;
+        this.shape = shape;
+    }
+}

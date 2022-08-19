23687b42b7809a14b79786d1526a0eeb1c15bbf5
Hugh <hughhanderson@icloud.com>
2022-08-19 20:23:02 +0900

    added color

--- app.js
+++ app.js
@@ -9,8 +9,15 @@ class Block {
         this.x = x;
         this.y = y;
     }
-    draw() {
-        ctx.fillRect(this.x*size, this.y*size, size, size);
+    draw(shape) {
+        switch(shape){
+            case 1: ctx.fillStyle = "red";
+            case 2: ctx.fillStyle = "red";
+            case 3: ctx.fillStyle = "red";
+            case 4: ctx.fillStyle = "red";
+            case 5: ctx.fillStyle = "red";
+        }
+        ctx.fillRect(this.x*size, this.y*size, size, size); 
     }
 }
 
@@ -22,12 +29,34 @@ class Mino {
         this.shape = shape;
     }
     calcBlocks() {
-        let blocks = [
-            new Block(-1,0),
-            new Block(0,0),
-            new Block(0,-1),
-            new Block(1,0),
-        ];
+        let blocks;
+        switch(this.shape){
+            case 1: blocks = [
+                new Block(-1,0),
+                new Block(0,0),
+                new Block(0,-1),
+                new Block(1,0),
+            ]; break;
+            case 2: blocks = [
+                new Block(-1,0),
+                new Block(0,0),
+                new Block(0,-1),
+                new Block(1,0),
+            ]; break;
+            case 3: blocks = [
+                new Block(-1,0),
+                new Block(0,0),
+                new Block(0,-1),
+                new Block(1,0),
+            ]; break;
+            case 4: blocks = [
+                new Block(-1,0),
+                new Block(0,0),
+                new Block(0,-1),
+                new Block(1,0),
+            ]; break; 
+        }
+        
         this.rot = (4+this.rot)%4;
         for(let r=0; r<this.rot; r++){
             blocks = blocks.map(b => new Block(-b.y, b.x));
@@ -37,7 +66,7 @@ class Mino {
     draw() {
         let blocks = this.calcBlocks();
         blocks.forEach(b => (b.x += this.x, b.y += this.y));
-        blocks.forEach(b => b.draw());
+        blocks.forEach(b => b.draw(this.shape));
     }
 }
 
@@ -69,10 +98,14 @@ class Field {
             [1,1,1,1,1,1,1,1,1,1,1,1],
         ];
     }
-    tileAt(x, y){
+    tileAt(x, y) {
         return this.tiles[y][x];
     }
+    setTile(x, y) {
+        this.tiles[y][x] = 1;
+    }
     draw() {
+        ctx.fillStyle = "black";
         for(let row=0; row<field_height; row++){
             for(let col=0; col<field_width; col++){
                 if(this.tileAt(col, row) == 1) ctx.fillRect(col*size, row*size, size, size);
@@ -83,20 +116,31 @@ class Field {
 
 class Game {
     constructor() {
-        this.mino = new Mino(5, 10, 0, 0);
         this.field = new Field();
+        this.mino = new Mino(5, 10, 0, 1);
         this.minoVx = 0;
         this.minoVy = 0;
         this.minoVr = 0;
     }
-    isMinoMovable(mino, field) {
-        console.log(mino)
+    newMino(mino, field){
         let blocks = mino.calcBlocks();
-        // blocks.dforEach(b => console.log(b.x+mino.x,b.y+mino.y));
-        blocks.forEach(b => console.log(field.tileAt(b.x+mino.x,b.y+mino.y)));
+        // console.log(mino);
+        // blocks.forEach(b => console.log(b.x+mino.x, b.y+mino.y));
+        if(blocks.some(b => field.tileAt(b.x+mino.x,b.y+mino.y) == 1)){
+            blocks.forEach(b => field.setTile(b.x+mino.x, b.y+mino.y-1));
+            this.mino = new Mino(5, 10, 0, 1);
+        }
+    }
+    isMinoMovable(mino, field) {
+        let blocks = mino.calcBlocks();
+        // blocks.forEach(b => console.log(b.y+mino.y));
         return blocks.every(b => field.tileAt(b.x+mino.x,b.y+mino.y) == 0);
     }
     proc(){
+        if(this.isNew){
+            this.newMino(this.mino, this.field);
+            this.isNew = false;
+        }
         if(this.minoVx != 0){
             let futureMino = new Mino(this.mino.x, this.mino.y, this.mino.rot, this.mino.shape);
             futureMino.x += this.minoVx;
@@ -108,6 +152,7 @@ class Game {
         if(this.minoVy != 0){
             let futureMino = new Mino(this.mino.x, this.mino.y, this.mino.rot, this.mino.shape);
             futureMino.y += this.minoVy;
+            this.newMino(futureMino, this.field);
             if (this.isMinoMovable(futureMino, this.field)){
                 this.mino.y += this.minoVy;
             }

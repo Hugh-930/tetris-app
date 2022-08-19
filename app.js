9b20683b700fd5b355129f86ec279f4d342eaeec
Hugh <hughhanderson@icloud.com>
2022-08-19 21:25:52 +0900

    added six more minos

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ var ctx = canvas.getContext("2d");
 var size = 30;
 var field_width = 12;
 var field_height = 22;
+var maxMino = 7;
+var random = Math.floor(Math.random()*maxMino);
+var initialX = 5;
+var initialY = 2;
+var initialR = 0;
+var initialSpeed = 0;
+
 
 class Block {
     constructor(x, y){
@@ -11,13 +18,17 @@ class Block {
     }
     draw(shape) {
         switch(shape){
-            case 1: ctx.fillStyle = "red";
-            case 2: ctx.fillStyle = "red";
-            case 3: ctx.fillStyle = "red";
-            case 4: ctx.fillStyle = "red";
-            case 5: ctx.fillStyle = "red";
+            case 0: ctx.fillStyle = "purple"; break;
+            case 1: ctx.fillStyle = "red"; break;
+            case 2: ctx.fillStyle = "green"; break;
+            case 3: ctx.fillStyle = "orange"; break;
+            case 4: ctx.fillStyle = "blue"; break;
+            case 5: ctx.fillStyle = "yellow"; break;
+            case 6: ctx.fillStyle = "aqua"; break;
         }
         ctx.fillRect(this.x*size, this.y*size, size, size); 
+        ctx.lineWidth = 10;
+        ctx.strokeRect(this.x*size, this.y*size, size, size);
     }
 }
 
@@ -31,29 +42,54 @@ class Mino {
     calcBlocks() {
         let blocks;
         switch(this.shape){
+            //T
+            case 0: blocks = [
+                new Block(-1,0),
+                new Block(0,0),
+                new Block(0,-1),
+                new Block(1,0),
+            ]; break;
+            //Z
             case 1: blocks = [
-                new Block(-1,0),
-                new Block(0,0),
+                new Block(-1,-1),
                 new Block(0,-1),
+                new Block(0,0),
                 new Block(1,0),
             ]; break;
+            //S
             case 2: blocks = [
-                new Block(-1,0),
                 new Block(0,0),
-                new Block(0,-1),
+                new Block(0,1),
                 new Block(1,0),
+                new Block(1,-1),
             ]; break;
+            //L
             case 3: blocks = [
-                new Block(-1,0),
                 new Block(0,0),
+                new Block(0,1),
                 new Block(0,-1),
-                new Block(1,0),
-            ]; break;
+                new Block(1,1),
+            ]; break; 
+            //J
             case 4: blocks = [
-                new Block(-1,0),
                 new Block(0,0),
+                new Block(0,1),
                 new Block(0,-1),
+                new Block(-1,1),
+            ]; break; 
+            //O
+            case 5: blocks = [
+                new Block(0,0),
+                new Block(0,1),
                 new Block(1,0),
+                new Block(1,1),
+            ]; break; 
+            //I
+            case 6: blocks = [
+                new Block(0,0),
+                new Block(0,1),
+                new Block(0,-1),
+                new Block(0,2),
             ]; break; 
         }
         
@@ -74,7 +110,7 @@ class Field {
     constructor(){
         //22*12
         this.tiles = [
-            [1,1,1,0,0,0,0,0,0,1,1,1],
+            [1,1,1,1,1,1,1,1,1,1,1,1],
             [1,0,0,0,0,0,0,0,0,0,0,1],
             [1,0,0,0,0,0,0,0,0,0,0,1],
             [1,0,0,0,0,0,0,0,0,0,0,1],
@@ -117,18 +153,17 @@ class Field {
 class Game {
     constructor() {
         this.field = new Field();
-        this.mino = new Mino(5, 10, 0, 1);
+        this.mino = new Mino(initialX, initialY, initialR, random);
         this.minoVx = 0;
         this.minoVy = 0;
         this.minoVr = 0;
     }
     newMino(mino, field){
+        random = Math.floor(Math.random()*maxMino);
         let blocks = mino.calcBlocks();
-        // console.log(mino);
-        // blocks.forEach(b => console.log(b.x+mino.x, b.y+mino.y));
         if(blocks.some(b => field.tileAt(b.x+mino.x,b.y+mino.y) == 1)){
             blocks.forEach(b => field.setTile(b.x+mino.x, b.y+mino.y-1));
-            this.mino = new Mino(5, 10, 0, 1);
+            this.mino = new Mino(initialX, initialY, initialR, random);
         }
     }
     isMinoMovable(mino, field) {
@@ -195,7 +230,12 @@ document.addEventListener("keypress", event => {
         ctx.clearRect(size, size, canvas.width-2*size, canvas.height-2*size);
         game.minoVr = +1;
         game.proc();
-    } 
+    }
+    if (keyName == "s") {
+        ctx.clearRect(size, size, canvas.width-2*size, canvas.height-2*size);
+        game.minoVy = +1;
+        game.proc();
+    }
 })
 
 let counter = 0;
@@ -206,10 +246,11 @@ const timerId = setInterval(function(){
         counter = 0;
     }
     game.proc();
-    if(keyName == "s"){ 
+    if(keyName == "p"){ 
         clearInterval(timerId)
     }
 }, 20)
+
 
 
 
